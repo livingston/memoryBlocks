f2ce38857cafc3581301035faf4cb3ddc6e7a16f
Livingston Samuel <livingston.samuel@gmail.com>
2013-08-03 00:57:49 +0530

    added unsupported browser check & message

--- js/app.js
+++ js/app.js
@@ -5,5 +5,13 @@
 */
 
 requirejs(['game/config', 'game/MemoryBlocks'], function (settings, MemoryBlocks) {
+  var body = document.body;
+
+  if (!document.createElement('canvas').getContext || !('bind' in Function.prototype)) {
+    body.style.cssText = 'text-align:center';
+    body.appendChild(document.createTextNode('Your browser doesn\'t support "Memory Blocks".'));
+    return null;
+  }
+
   var mb = new MemoryBlocks(settings);
 });
